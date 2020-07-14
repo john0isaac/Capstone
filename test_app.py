e4da56fdb5a49746c16ac61f1135f25c34b12823
john0isaac <johnaziz269@gmail.com>
2020-07-14 07:23:16 +0200

    Database url

--- test_app.py
+++ test_app.py
@@ -15,7 +15,7 @@ class JohnTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "capstone_test"
-        self.database_path = "postgres://{}@{}/{}".format('postgres:davinchi2020','0.0.0.0:5432', self.database_name)
+        self.database_path = os.environ['DATABASE_URL']
         setup_db(self.app, self.database_path)
 
         self.new_person = {
